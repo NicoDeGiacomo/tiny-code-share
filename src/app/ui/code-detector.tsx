91774f486a5c627416a41d3bf5c86e64e4aad88f
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2025-03-24 18:13:29 -0300

    used finite state machine to manage toggles changes on code detection

--- src/app/ui/code-detector.tsx
+++ src/app/ui/code-detector.tsx
@@ -9,6 +9,7 @@ import {useDebouncedCallback} from "use-debounce";
 
 
 const SEARCH_PARAM_IS_EDITABLE = "isEditable"
+const SEARCH_PARAM_AUTO_DETECT = "autoDetect"
 const SEARCH_PARAM_CODE = "code"
 const SEARCH_PARAM_LANG = "lang"
 
@@ -23,14 +24,54 @@ export default function CodeDetector() {
     return new URLSearchParams(hash);
   };
 
-  const [autoDetect, setAutoDetect] = useState(true);
-  const [language, setLanguage] = useState(DEFAULT_LANGUAGE);
+  // TODO: para evitar dos ifs directamente llamar a una funcion distinta segun el estado
+  // usar una maquina de estado finito que determina que funcion llamar cada vez que se cambia
+  // alguna config tomando cada combinacion posibles
+  // ejemplo: isEditable && autoDetect: handleCodeChangeEditableDetect
+  const defaultHandleCode = (value?: string) => {
+    console.log('defaultHandleCode')
+    if (isEditable) {
+      const codeValue = value || ''
+      setCode(codeValue);
+      if (autoDetect) {
+        const detectedLang = hljs.highlightAuto(codeValue).language || DEFAULT_LANGUAGE;
+        setLanguage(detectedLang);
+      }
+    }
+  };
+
+  const handleCodeChangeEditableAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeEditableAutoDetect')
+    const codeValue = value || ''
+    setCode(codeValue);
+    const detectedLang = hljs.highlightAuto(codeValue).language || DEFAULT_LANGUAGE;
+    setLanguage(detectedLang);
+  };
+
+  const handleCodeChangeEditableNotAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeEditableNotAutoDetect')
+    setCode(value || '');
+  };
+
+  const handleCodeChangeNotEditableNotAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeNotEditableNotAutoDetect')
+    // Do nothing
+  };
+
+  const handleCodeChangeNotEditableAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeNotEditableAutoDetect')
+    // Do nothing
+  };
+
   const [linkCopied, setLinkCopied] = useState(false);
 
   const searchParams = getHashParams();
-  const compressedCode = searchParams.get(SEARCH_PARAM_CODE) ?? '';
-  const [code, setCode] = useState(LZString.decompressFromEncodedURIComponent(compressedCode));
-  const [isEditable, setIsEditable] = useState(searchParams.get(SEARCH_PARAM_IS_EDITABLE) ? searchParams.get(SEARCH_PARAM_IS_EDITABLE) === "true" : false);
+  const [code, setCode] = useState(LZString.decompressFromEncodedURIComponent(searchParams.get(SEARCH_PARAM_CODE) ?? ''));
+  const [isEditable, setIsEditable] = useState(searchParams.get(SEARCH_PARAM_IS_EDITABLE) === "true");
+  const [autoDetect, setAutoDetect] = useState(searchParams.get(SEARCH_PARAM_AUTO_DETECT) === "true");
+  const [language, setLanguage] = useState(searchParams.get(SEARCH_PARAM_LANG) || DEFAULT_LANGUAGE);
+
+  const [onCodeChangeCallback, setOnChangeCodeCallback] = useState<(value?: string) => void>(() => defaultHandleCode);
 
   const createShareableURL = (pSearchParams: URLSearchParams ) => {
     return `${window.location.origin}#${pSearchParams}`
@@ -47,6 +88,7 @@ export default function CodeDetector() {
       searchParams.set(SEARCH_PARAM_CODE, compressedCode)
       searchParams.set(SEARCH_PARAM_LANG, language)
       searchParams.set(SEARCH_PARAM_IS_EDITABLE, isEditable.toString())
+      searchParams.set(SEARCH_PARAM_AUTO_DETECT, autoDetect.toString())
 
       navigator.clipboard.writeText(createShareableURL(searchParams))
           .then(() => {
@@ -57,27 +99,30 @@ export default function CodeDetector() {
           );
   };
 
-  // TODO: para evitar dos ifs directamente llamar a una funcion distinta segun el estado
-  // usar una maquina de estado finito que determina que funcion llamar cada vez que se cambia
-  // alguna config tomando cada combinacion posibles
-  // ejemplo: isEditable && autoDetect: handleCodeChangeEditableDetect
-  const handleCodeChange = (value?: string) => {
-      if (isEditable) {
-        const codeValue = value || ''
-        setCode(codeValue);
-        if (autoDetect) {
-            const detectedLang = hljs.highlightAuto(codeValue).language || DEFAULT_LANGUAGE;
-            setLanguage(detectedLang);
-        }
-      }
-  };
-
   const toggleEditable = (event: ChangeEvent<HTMLInputElement>) => {
-      setIsEditable(event.target.checked)
+    setIsEditable(event.target.checked)
+    if (event.target.checked && autoDetect) {
+      setOnChangeCodeCallback(() => handleCodeChangeEditableAutoDetect)
+    } else if (event.target.checked && !autoDetect) {
+      setOnChangeCodeCallback(() => handleCodeChangeEditableNotAutoDetect)
+    } else if (!event.target.checked && autoDetect) {
+      setOnChangeCodeCallback(() => handleCodeChangeNotEditableAutoDetect)
+    } else if (!event.target.checked && !autoDetect) {
+      setOnChangeCodeCallback(() => handleCodeChangeNotEditableNotAutoDetect)
+    }
   };
 
   const toggleAutoDetect = (event: ChangeEvent<HTMLInputElement>) => {
     setAutoDetect(event.target.checked);
+    if (event.target.checked && isEditable) {
+      setOnChangeCodeCallback(() => handleCodeChangeEditableAutoDetect)
+    } else if (event.target.checked && !isEditable) {
+      setOnChangeCodeCallback(() => handleCodeChangeNotEditableAutoDetect)
+    } else if (!event.target.checked && isEditable) {
+      setOnChangeCodeCallback(() => handleCodeChangeEditableNotAutoDetect)
+    } else if (!event.target.checked && !isEditable) {
+      setOnChangeCodeCallback(() => handleCodeChangeNotEditableNotAutoDetect)
+    }
   };
 
   const MostUsedLangs = () => {
@@ -105,7 +150,6 @@ export default function CodeDetector() {
                       className="relative w-9 h-5 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
                     <span className="ms-3 text-sm font-medium">Detect</span>
                 </label>
-
                 <select className={cn("border border-black p-1 bg-gray-100 disabled:text-gray-600 disabled:bg-gray-300")} 
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
@@ -124,7 +168,7 @@ export default function CodeDetector() {
                 <Editor
                   height="80vh"
                   theme="vs-dark"
-                  onChange={handleCodeChange}
+                  onChange={onCodeChangeCallback}
                   options={{readOnly: !isEditable}}
                   language={language}
                   value={code}
@@ -141,8 +185,6 @@ export default function CodeDetector() {
             </div>
 
         </div>
-
-
     </>
   );
 }
