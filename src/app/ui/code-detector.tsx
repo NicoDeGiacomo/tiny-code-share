f523efe566efed432b0756bd22266aa5c0eb9a56
AgustinLeguizamon <38507913+AgustinLeguizamon@users.noreply.github.com>
2025-03-26 12:39:07 -0300

    Enhancement/detect performance (#6)
    * variables and useState refactors
    
    * used finite state mach ine to manage toggles changes on code detection
    
    * refactor state machine, add not allowed cursor on disabled select
    
    * state machine refactor

--- src/app/ui/code-detector.tsx
+++ src/app/ui/code-detector.tsx
@@ -2,130 +2,167 @@
 
 import {Editor} from "@monaco-editor/react";
 import LZString from "lz-string";
-import {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import hljs from "highlight.js";
 import {cn} from "@/app/utils";
 import {useDebouncedCallback} from "use-debounce";
 
 
+const SEARCH_PARAM_IS_EDITABLE = "isEditable"
+const SEARCH_PARAM_AUTO_DETECT = "autoDetect"
+const SEARCH_PARAM_CODE = "code"
+const SEARCH_PARAM_LANG = "lang"
+
+const DEFAULT_LANGUAGE = "javascript"
+
+const VISIBLE_TIME = 3000;
+
 export default function CodeDetector() {
-    const [autoDetect, setAutoDetect] = useState(true);
-    const [language, setLanguage] = useState('javascript');
-    const [linkCopied, setLinkCopied] = useState(false);
+  
+  const getHashParams = () => {
+    const hash = window.location.hash.substring(1);
+    return new URLSearchParams(hash);
+  };
 
-    const getHashParams = () => {
-        const hash = window.location.hash.substring(1);
-        return new URLSearchParams(hash);
-    };
+  const handleSetOnChangeCodeCallback = (isEditable: boolean, autoDetect: boolean): ((value?: string) => void) => {
+    if (isEditable && autoDetect) {
+      return handleCodeChangeEditableAutoDetect;
+    } else if (isEditable && !autoDetect) {
+      return handleCodeChangeEditableNotAutoDetect;
+    } else if (!isEditable && autoDetect) {
+      return handleCodeChangeNotEditableAutoDetect;
+    } else {
+      return handleCodeChangeNotEditableNotAutoDetect;
+    }
+  }
 
-    const searchParams = getHashParams();
-    const compressedCode = searchParams.get("code") ?? '';
-    const [code, setCode] = useState(LZString.decompressFromEncodedURIComponent(compressedCode));
-    const [isEditable, setIsEditable] = useState(searchParams.get("isEditable") ? searchParams.get("isEditable") === "true" : false);
+  const handleCodeChangeEditableAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeEditableAutoDetect')
+    const codeValue = value || ''
+    setCode(codeValue);
+    const detectedLang = hljs.highlightAuto(codeValue).language || DEFAULT_LANGUAGE;
+    setLanguage(detectedLang);
+  };
 
-    const VISIBLE_TIME = 3000;
-    const handleLinkCopied = useDebouncedCallback(() => {
-        setLinkCopied(false)
-    }, VISIBLE_TIME);
+  const handleCodeChangeEditableNotAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeEditable')
+    setCode(value || '');
+  };
 
-    const handleShare = () => {
-        const compressedCode = LZString.compressToEncodedURIComponent(code);
+  const handleCodeChangeNotEditableNotAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeNotEditableNotAutoDetect')
+    // Do nothing
+  };
 
-        const searchParams = new URLSearchParams(window.location.hash.substring(1));
-        searchParams.set("code", compressedCode)
-        searchParams.set("lang", language)
-        searchParams.set("isEditable", isEditable.toString())
+  const handleCodeChangeNotEditableAutoDetect = (value?: string) => {
+    console.log('handleCodeChangeNotEditableAutoDetect')
+    // Do nothing
+  };
 
-        const shareableURL = `${window.location.origin}#${searchParams}`;
-        navigator.clipboard.writeText(shareableURL)
-            .then(() => {
-                console.log('Copied')
-                setLinkCopied(true)
-                handleLinkCopied()
-              }
-            );
-    };
+  const [linkCopied, setLinkCopied] = useState(false);
 
-    const handleCodeChange = (value?: string) => {
-        if (isEditable) {
-            setCode(value || '');
-            if (autoDetect) {
-                const detectedLang = hljs.highlightAuto(value || '').language || 'javascript';
-                setLanguage(detectedLang);
+  const searchParams = getHashParams();
+  const [code, setCode] = useState(LZString.decompressFromEncodedURIComponent(searchParams.get(SEARCH_PARAM_CODE) ?? ''));
+  const [isEditable, setIsEditable] = useState(searchParams.get(SEARCH_PARAM_IS_EDITABLE) === "true");
+  const [autoDetect, setAutoDetect] = useState(searchParams.get(SEARCH_PARAM_AUTO_DETECT) === "true");
+  const [language, setLanguage] = useState(searchParams.get(SEARCH_PARAM_LANG) || DEFAULT_LANGUAGE);
+
+  const [onCodeChangeCallback, setOnChangeCodeCallback] = useState<(value?: string) => void>(() => handleSetOnChangeCodeCallback(isEditable, autoDetect));
+
+  const createShareableURL = (pSearchParams: URLSearchParams ) => {
+    return `${window.location.origin}#${pSearchParams}`
+  }
+
+  const showLinkCopied = useDebouncedCallback(() => {
+      setLinkCopied(false)
+  }, VISIBLE_TIME);
+
+  const handleShare = () => {
+      const compressedCode = LZString.compressToEncodedURIComponent(code);
+
+      const searchParams = getHashParams();
+      searchParams.set(SEARCH_PARAM_CODE, compressedCode)
+      searchParams.set(SEARCH_PARAM_LANG, language)
+      searchParams.set(SEARCH_PARAM_IS_EDITABLE, isEditable.toString())
+      searchParams.set(SEARCH_PARAM_AUTO_DETECT, autoDetect.toString())
+
+      navigator.clipboard.writeText(createShareableURL(searchParams))
+          .then(() => {
+              setLinkCopied(true)
+              showLinkCopied()
             }
-        }
-    };
+          );
+  };
 
-    const toggleEditable = (checked: boolean) => {
-        setIsEditable(checked)
-    };
+  const toggleEditable = (event: ChangeEvent<HTMLInputElement>) => {
+    setIsEditable(event.target.checked)
+    setOnChangeCodeCallback(() => handleSetOnChangeCodeCallback(event.target.checked, autoDetect));
+  };
 
-    const toggleAutoDetect = () => {
-        setAutoDetect(!autoDetect);
-        if (!autoDetect) {
-            const detectedLang = hljs.highlightAuto(code).language || 'javascript';
-            setLanguage(detectedLang);
-        }
-    };
+  const toggleAutoDetect = (event: ChangeEvent<HTMLInputElement>) => {
+    setAutoDetect(event.target.checked);
+    setOnChangeCodeCallback(() => handleSetOnChangeCodeCallback(isEditable, event.target.checked));
+  };
 
-    return (
-        <>
-          <div className="flex flex-col items-end relative">
-              <div className="flex items-center space-x-4 mr-1">
-                  <label className="inline-flex items-center cursor-pointer border border-black p-1">
-                      <input type="checkbox" onChange={() => toggleEditable(!isEditable)} checked={isEditable} className="sr-only peer"/>
-                      <div
-                        className="relative w-9 h-5 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-                      <span className="ms-3 text-sm font-medium">Edit</span>
-                  </label>
+  const MostUsedLangs = () => {
+    return <>                
+      <option>javascript</option>
+      <option>python</option>
+      <option>java</option>
+    </>
+  }
 
-                  <label className="inline-flex items-center cursor-pointer border border-black p-1">
-                      <input type="checkbox" onChange={toggleAutoDetect} className="sr-only peer"/>
-                      <div
-                        className="relative w-9 h-5 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-                      <span className="ms-3 text-sm font-medium">Detect</span>
-                  </label>
-
-                  <select className="border border-black p-1 bg-gray-100" value={language}
-                          onChange={(e) => setLanguage(e.target.value)}
-                          disabled={autoDetect}>
-                      <option>javascript</option>
-                      <option>python</option>
-                      <option>java</option>
-                      {hljs.listLanguages().map(
-                        (lang) => (
-                          <option key={lang} value={lang}>
-                              {lang}
-                          </option>
-                        )
-                      )}
-                  </select>
-              </div>
-
-              <div className="w-full h-full p-1">
-                  <Editor
-                    height="80vh"
-                    theme="vs-dark"
-                    onChange={handleCodeChange}
-                    options={{readOnly: !isEditable}}
-                    language={language}
-                    value={code}
-                  />
-              </div>
-
-              <div className="flex items-center gap-2 m-2">
-                  <p className={cn('text-green-500', {'hidden': !linkCopied})}>Link copied!</p>
-                  <button
-                    onClick={handleShare}
-                    className="h-8 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                  >
-                      Get Link
-                  </button>
-              </div>
-
+  return (
+    <>
+      <div className="flex flex-col items-end relative">
+          <div className="flex items-center space-x-4 mr-1">
+              <label className="inline-flex items-center cursor-pointer border border-black p-1">
+                  <input type="checkbox" onChange={toggleEditable} checked={isEditable} className="sr-only peer"/>
+                  <div
+                    className="relative w-9 h-5 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                  <span className="ms-3 text-sm font-medium">Edit</span>
+              </label>
+              <label className="inline-flex items-center cursor-pointer border border-black p-1">
+                  <input type="checkbox" onChange={toggleAutoDetect} checked={autoDetect} className="sr-only peer"/>
+                  <div
+                    className="relative w-9 h-5 bg-gray-200 rounded-full dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+                  <span className="ms-3 text-sm font-medium">Detect</span>
+              </label>
+              <select className={cn("border border-black p-1 bg-gray-100 disabled:text-gray-600 disabled:bg-gray-300 disabled:cursor-not-allowed")} 
+              value={language}
+              onChange={(e) => setLanguage(e.target.value)}
+              disabled={autoDetect}>
+                <MostUsedLangs/>
+                {hljs.listLanguages().map(
+                  (lang) => (
+                    <option key={lang} value={lang}>
+                        {lang}
+                    </option>
+                  )
+                )}
+              </select>
+          </div>
+          <div className="w-full h-full p-1">
+              <Editor
+                height="80vh"
+                theme="vs-dark"
+                onChange={onCodeChangeCallback}
+                options={{readOnly: !isEditable}}
+                language={language}
+                value={code}
+              />
+          </div>
+          <div className="flex items-center gap-2 m-2">
+              <p className={cn('text-green-600', {'hidden': !linkCopied})}>Link copied to clipboard!</p>
+              <button
+                onClick={handleShare}
+                className="h-8 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium text-sm px-5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              >
+                  Generate Code Link
+              </button>
           </div>
 
-
-      </>
-    );
+      </div>
+    </>
+  );
 }
